23fc67258b553227b82dc2d56dab991ba34a275f
Smiri-s <stela.hasambelliu@finetuning.it>
2020-11-06 16:45:58 +0100

    Implementing the destroy method in the Portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,4 +43,14 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.find(params[:id])
       end 
 
-end
+      def destroy
+        @portfolio_item = Portfolio.find(params[:id])
+        @portfolio_item.destroy
+     respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully removed.' }
+      
+        end
+      end
+    end
+
+
